e04fc44ca7b1facdd4e0e45af61e37b1ff31b7cf
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-02-08 15:30:09 +0200

    Coordinate reader per click on the grid

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded' , () => {
     let objShips = {shipTypes : {} , shipOrientation : "Vertical"};
     const shipOrientation = document.getElementById('shipOrientation');
     const shipTypes = document.querySelectorAll('.button-section div');
-    let current_ShipType = null;
 
     shipOrientation.addEventListener('change' , (event) => {
         const chosenOrientation = event.target.value;
@@ -10,16 +9,34 @@ document.addEventListener('DOMContentLoaded' , () => {
         console.log(objShips)
     })
 
+    const getNumberOfPointsPerShip = (str) =>{
+        const value = str.indexOf(" ") + 2;
+        const nameOfShip = str.substring(0 , str.indexOf(" "));
+        return {name : nameOfShip , value: Number(str[value])}
+    }
+
     shipTypes.forEach(ship => {
         ship.addEventListener('click', (event)=>{
             const clicked_Div = event.currentTarget;
-            const getNumberOfPointsPerShip = (str) =>{
-                const value = str.indexOf(" ") + 2;
-                const nameOfShip = str.substring(0 , str.indexOf(" "));
-                return {name : nameOfShip , value: Number(str[value])}
-            }
             objShips.shipTypes = getNumberOfPointsPerShip(clicked_Div.textContent);
             console.log(objShips);   
         })
     })
+
+    //ship placement logic
+    const playerTable = document.getElementById('left');
+
+    const convertDigitsToLetter = (value) =>{
+        return String.fromCharCode(64 + value);
+    }
+
+    playerTable.addEventListener("click" , (event) => {
+        const cell = event.target;
+        
+        const row = cell.parentElement.rowIndex;
+        const col = cell.cellIndex;
+
+        console.log(convertDigitsToLetter(col)+ " "+ row );
+    })
+
 })
