2941701553fccc38a5f7ef8a7d254fba8201c2c8
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-02-15 23:15:42 +0200

    Added Ship sinking logic to the game for better communication to the server'

--- Modules.js
+++ Modules.js
@@ -1,9 +1,9 @@
 const obj_AllShips = [
-  { shipName: "Carrier" , length:5},
-  { shipName: "Battleship" , length:4},
-  { shipName: "Cruiser" , length:3},
-  { shipName: "Submarine" , length:3},
-  { shipName: "Destroyer" , length:2}
+  { shipName: "Carrier" , length:5, health: 5, placement : null , isVertical : null},
+  { shipName: "Battleship" , length:4, health: 4 , placement : null, isVertical: null},
+  { shipName: "Cruiser" , length:3 , health: 3 , placement : null, isVertical : null},
+  { shipName: "Submarine" , length:3 , health: 3, placement : null, isVertical : null},
+  { shipName: "Destroyer" , length:2, health: 2, placement : null, isVertical : null}
 ];
 
 const getNumberOfPointsPerShip = (str) => {
@@ -18,6 +18,15 @@ const getNumberOfPointsPerShip = (str) => {
     }
   }
 };
+
+const initialiseShips = (shipName,col, row, orientation) =>{
+  for(let i = 0; i < obj_AllShips.length; i++){
+    if(obj_AllShips[i].shipName === shipName){
+      obj_AllShips[i].placement = convertDigitsToLetter(col) + row;
+      obj_AllShips[i].isVertical = (orientation === "Vertical");
+    }
+  }
+}
 
 const convertDigitsToLetter = (value) => {
     return String.fromCharCode(64 + value);
@@ -43,19 +52,38 @@ const checkPlacement = (grid , obj , col, row) => {
     return true;
 }
 
-/*const detectSunk = (grid, gridRow , gridCol) =>{
-  //horizontal movement
-  for(let row = 0; row < grid.length; row++){
-    
+const findShipByCoordinate = (row, col) =>{
+  for(let ship of obj_AllShips){
+    const startCol =  ship.placement.charCodeAt(0) - 65;
+    const startRow = parseInt(ship.placement.substring(1)) - 1;
+    for(let i = 0; i < ship.length; i++){
+      const shipsRow = ship.isVertical ? startRow + i : startRow;
+      const shipsCol = ship.isVertical ? startCol : startCol + i;
+      if(shipsRow === row && shipsCol === col) return ship;
+    }  
   }
+  return null;
+}
 
-  //Column movement
-  for(let col = 0; col <grid[0].length; col++){
-    for(let row = 0; row < grid.length; row++){
+const detectSunk = (grid, shipObj) =>{
+  const startValue = shipObj.placement;
+  const length = ship.length;
+  const isVertical = ship.isVertical;
 
+  const startCol = startValue.charCodeAt(0) - 65;
+  const startRow = parseInt(startValue.substring(1)) - 1; //This is because the values technically start from 0
+
+  for(let i = 0; i < length; i++){
+    const row = isVertical ? startRow + i: startRow;
+    const col = isVertical ? startCol : startCol + i;
+
+    if(grid[row][col] !== "XX"){
+      return false;
     }
   }
-}*/
+  console.log(ship.shipName + " has been sunk");
+  return true; 
+};
 
 const updateTable = (grid, table, isHiddenDisplay) => {
   for (let iterateThroughRows = 0; iterateThroughRows < grid.length; iterateThroughRows++) {
@@ -81,5 +109,5 @@ const updateTable = (grid, table, isHiddenDisplay) => {
   }
 };
 
-export {updateTable,convertDigitsToLetter,getNumberOfPointsPerShip,checkPlacement};
+export {updateTable,convertDigitsToLetter,getNumberOfPointsPerShip,checkPlacement, initialiseShips, detectSunk , findShipByCoordinate};
 
